7518976def8306bc8a357192b3612aecdde4511d
Nicholas Dobie <nick@dobie.me>
2020-03-25 22:48:39 -0500

    using json list

--- packages/@quote-deploy/server-ds-quotes/src/quotes/quotes.controller.ts
+++ packages/@quote-deploy/server-ds-quotes/src/quotes/quotes.controller.ts
@@ -1,11 +1,14 @@
 import { Controller } from '@nestjs/common'
 import { Quote } from './models/quote.model'
 import { MessagePattern } from '@nestjs/microservices'
+import { QuotesService } from './quotes.service'
 
 @Controller()
 export class QuotesController {
+  constructor(private readonly quotesService: QuotesService) {}
+
   @MessagePattern('getRandomQuote')
   public getRandomQuote(): Quote {
-    return {id: '1', quote: 'BETTER', author: 'NICK'};
+    return this.quotesService.getRandomQuote()
   }
 }
--- packages/@quote-deploy/server-ds-quotes/src/quotes/quotes.service.ts
+++ packages/@quote-deploy/server-ds-quotes/src/quotes/quotes.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import * as data from './data/quotes.json'
+import { Quote } from './models/quote.model';
 
 @Injectable()
-export class QuotesService {}
+export class QuotesService {
+  public getRandomQuote(): Quote {
+    const cloneArray = [...data]
+    cloneArray.sort(() => Math.round((Math.random() * 2) - 1))
+    return {
+      id: `${Date.now().toString(16)}`,
+      ...cloneArray.shift()
+    }
+  }
+}
